09387f5d7d7bf9317d35e31fb82b26445cf78576
Luke Van In <lukevanin@gmail.com>
2021-06-16 13:58:19 +0200

    Add mock transport

---
+++ Transport/MockTransport.swift
@@ -0,0 +1,41 @@
+//
+//  MockTransport.swift
+//  Transport
+//
+//  Created by Luke Van In on 2021/06/16.
+//
+
+import Foundation
+import Combine
+
+///
+/// Returns a predefined response. Used to replace concrete REST protocol types for testing.
+///
+public final class MockTransport: RESTTransport {
+    
+    typealias Get = (_ path: String, _ parameters: [URLQueryItem]) throws -> String
+
+    var decoder: JSONDecoder = JSONDecoder()
+    var mockGet: Get?
+    
+    public func get<T>(path: String, parameters: [URLQueryItem]) -> AnyPublisher<T, Error> where T : Decodable {
+        serialize() { [mockGet] in
+            try mockGet!(path, parameters)
+        }
+    }
+    
+    private func serialize<T>(operation: @escaping () throws -> String) -> AnyPublisher<T, Error> where T: Decodable {
+        Future { [decoder] completion in
+            do {
+                let string = try operation()
+                let data = string.data(using: .utf8)!
+                let instance = try decoder.decode(T.self, from: data)
+                completion(.success(instance))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
